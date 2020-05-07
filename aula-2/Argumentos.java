eab6214197aabd53935b7ecbbe03a9558f6332ab
Bruno Bueno da Silva <fvd_bsilva@uolinc.com>
2020-05-07 16:09:02 -0300

    aula-2

---
+++ aula-2/Argumentos.java
@@ -0,0 +1,7 @@
+public class Argumentos {
+
+    public static void main(String[] args) {
+  
+        System.out.println( "Você digitou " + args[0]);
+    }
+}
---
+++ aula-2/Argumentos2.java
@@ -0,0 +1,7 @@
+public class Argumentos2 {
+
+    public static void main(String[] args) {
+  
+        System.out.println( "Hello World e seu argumento é " + args[0]);
+    }
+}
